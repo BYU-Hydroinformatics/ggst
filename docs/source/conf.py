7412f631699a6201d4943e2d192cca858127ef84
Norm Jones <njones61@gmail.com>
2023-04-19 17:13:47 -0600

    Update conf.py
    testing page width again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,4 +34,4 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-html_theme_options = {'body_max_width': '70%'}
+html_theme_options = {'body_max_width': '100%'}
